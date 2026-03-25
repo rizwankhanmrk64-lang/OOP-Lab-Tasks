fbb6470b47068095aacffac88eb9795310e31469
Rizwan Khan <rizwankhan.mrk64@gmail.com>
2026-03-25 22:31:56 +0500

    addition/subtraction/multiplication

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    public static void main(String args []){
+
+//Addition
+
+        int a = 15;
+        int b = 20;
+        //int sum = a+b;
+       // System.out.println( sum);
+
+       //subtraction
+
+     //  int subtract = b-a;
+      // System.out.println(subtract);
+    
+
+
+//Multiplication
+
+int multi = a*b;
+System.out.println (multi);
+
+
+    }
+}
